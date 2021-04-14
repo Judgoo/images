c1ecc8e26fcbddeeda53da698bdf56b7014db90a
lengthmin <lengthmin@gmail.com>
2021-04-14 13:33:35 +0000

    feat: rename python

--- generator/python.py
+++ generator/python.py
@@ -1,10 +1,10 @@
 from .helper.base import DockerFile
 
 
-py39 = DockerFile("python3.9.4", base_img="python:3.9.4-alpine3.13")
+py39 = DockerFile("python3.9", base_img="python:3.9-alpine3.13")
 py39.add_judger()
 
-py38_w = DockerFile("python3.8-with-packages")
+py38_w = DockerFile("python3-with-packages")
 py38_w.add_packages("python3 py3-pandas py3-numpy")
 
 ALL_IMAGES = [py39, py38_w]
