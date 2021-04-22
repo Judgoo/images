42d7c9b96d2292ab5ad94b43b3a0f515d6843af9
lengthmin <lengthmin@gmail.com>
2021-04-22 12:52:34 +0000

    fix: python 镜像判题添加工具

--- generator/python-debian.py
+++ generator/python-debian.py
@@ -3,9 +3,10 @@ from .helper import DebianBaseDockerFile
 
 py27 = DebianBaseDockerFile("python2.7", base_img="python:2.7.18-slim-buster")
 py27.RUN = "pip install --no-cache numpy pandas"
-
+py27.add_judger()
 
 py39 = DebianBaseDockerFile("python3.9", base_img="python:3.9-slim-buster")
 py39.RUN = "pip install --no-cache numpy pandas"
+py39.add_judger()
 
 ALL_IMAGES = [py27, py39]
